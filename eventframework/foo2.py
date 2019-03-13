3559cdb5b3cb3e0b4f8f7e97de2d84e4a222c515
ankit <ankitarora121@gmail.com>
2019-03-14 00:36:29 +0530

    Make things more concrete

--- eventframework/foo2.py
+++ eventframework/foo2.py
@@ -8,17 +8,17 @@ fixed_rate_order_blueprint_definition = {
     "name": "fixed_rate_order_blueprint_definition",
     "conditions": [
         {
-            "event": "new_order",
+            "events": ["new_order"],
             "action": "action_check_for_deposit",
             "adapter": "adapter_check_deposit_when_new_order"
         },
         {
-            "event": "deposit_status",
+            "events": ["deposit_status"],
             "action": "from_holding_branch",
             "adapter": "adapter_from_holding_when_deposit_status"
         },
         {
-            "event": "deposit_status",
+            "events": ["deposit_status"],
             "action": "to_holding_branch",
             "adapter": "adapter_to_holding_branch_when_deposit_status"
         }
@@ -30,10 +30,29 @@ class InvalidBlueprintException(Exception):
     pass
 
 
+class Event:
+    pass
+
+
+class Action:
+    pass
+
+
+class Adapter:
+    pass
+
+
+@dataclass
+class BlueprintCondition:
+    events: Event
+    action: Action
+    adapter: Adapter
+
+
 @dataclass
 class Blueprint:
     name: str
-    conditions: List[Dict]
+    conditions: List[BlueprintCondition]
 
 
 @dataclass
@@ -50,7 +69,7 @@ class BlueprintExecution:
 
 
 class BlueprintManager:
-    blueprint_condition_component_names = ['event', 'action', 'adapter']
+    blueprint_condition_attribute_names = dict(BlueprintCondition).keys()
 
     def __init__(self, config):
         BlueprintManager.validate_config(config)
@@ -61,7 +80,7 @@ class BlueprintManager:
     def validate_config(config):
         if 'namespace' not in config:
             raise InvalidBlueprintException(f"Config must have 'namespace' key")
-        for each in BlueprintManager.blueprint_condition_component_names:
+        for each in BlueprintManager.blueprint_condition_attribute_names:
             if each not in config['namespace']:
                 raise InvalidBlueprintException(f"Namespace must have key '{each}'")
 
@@ -77,21 +96,39 @@ class BlueprintManager:
             raise InvalidBlueprintException("Blueprint definition must have key 'conditions'")
 
         for i, condition in enumerate(blueprint_conditions):
-            for condition_component in self.blueprint_condition_component_names:
+            for condition_attribute_name in self.blueprint_condition_attribute_names:
 
-                component_name = condition.get(condition_component)
+                component_name = condition.get(condition_attribute_name)
                 if not component_name:
                     raise InvalidBlueprintException(f"Component '{component_name}' must exist inside {i}th condition in blueprint_definition")
 
-                component_object = self.config['namespace'][condition_component].get(component_name)
+                component_object = self.config['namespace'][condition_attribute_name].get(component_name)
                 if not component_object:
-                    raise InvalidBlueprintException(f"As per configured namespace, no mapping found for {condition_component} {component_name}")
+                    raise InvalidBlueprintException(f"As per configured namespace, no mapping found for {condition_attribute_name} {component_name}")
+
+    def _objectify_condition(self, condition_definition: Dict) -> BlueprintCondition:
+        def _objectify(attribute, component_name):
+            return self.config['namespace'][attribute][component_name]
+
+        mydict = {}
+        for attribute, component_name in condition_definition:
+            mydict[attribute] = _objectify(attribute, component_name)
+
+        return BlueprintCondition(**mydict)
+
+    def _convert_definitions_to_concrete(self, blueprint_definition) -> Blueprint:
+        conditions = []
+        for definition in blueprint_definition['conditions']:
+            condition: BlueprintCondition = self._objectify_condition(definition)
+            conditions.append(condition)
+        return Blueprint(blueprint_definition['name'], conditions)
 
     def add_blueprint(self, blueprint_definition):
         self._validate_blueprint_definition(blueprint_definition)
-        if blueprint_definition['name'] in self.live_blueprints_by_name:
+        blueprint = self._convert_definitions_to_concrete(blueprint_definition)
+        if blueprint.name in self.live_blueprints_by_name:
             raise InvalidBlueprintException(f"Blueprint with name {blueprint_definition['name']} is already added")
-        self.live_blueprints_by_name[blueprint_definition['name']] = Blueprint(blueprint_definition['name'], blueprint_definition['conditions'])
+        self.live_blueprints_by_name[blueprint.name] = blueprint
 
 
 class BlueprintExecutionStore:
@@ -139,18 +176,23 @@ class BlueprintExecutionManager:
 class BlueprintExecutor:
     DEFAULT_POLL_TIME = 10
 
-    def __init__(self, blueprint_execution_store: BlueprintExecutionStore, event_bus: EventBus):
+    def __init__(self, blueprint_manager: BlueprintManager, blueprint_execution_store: BlueprintExecutionStore, event_bus: EventBus):
         self.blueprint_execution_store = blueprint_execution_store
         self.event_bus = event_bus
+        self.blueprint_manager = blueprint_manager
 
     def run(self):
         while True:
-            time.sleep(self.DEFAULT_POLL_TIME)
             blueprint_execution: BlueprintExecution = self.blueprint_execution_store.get_execution_to_process()
 
             blueprint_execution_id = blueprint_execution.execution_id
             conditions_to_process = blueprint_execution.blueprint.conditions
             for condition in conditions_to_process:
+                events = self._get_necessary_events(condition)
+                if not events:
+                    break
+                conditions_with_objects = self.blueprint_manager
+                self._process_condition(blueprint_execution, condition)
                 """
                 Narrative:
                     Check event bus for latest events of condition['events']
@@ -161,6 +203,14 @@ class BlueprintExecutor:
                         send output of adapter to action
                         mark condition successfully evaluated
                 """
+
+            time.sleep(self.DEFAULT_POLL_TIME)
+
+    def _get_necessary_events(self, condition):
+        pass
+
+    def _process_condition(self, blueprint_execution: BlueprintExecution, condition):
+        pass
 
 
 def add_new_order_for_execution():
