188c0245f1abbaa7f1efa9253e88ded355699dba
ankit <ankitarora121@gmail.com>
2019-03-19 06:58:35 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='blue_event_framework',
-    version='0.0.1',
+    version='0.1.0',
     author='Coinswitch',
     author_email='dev@coinswitch.co',
     description='dummy description',
