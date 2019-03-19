3e84f34b598fd6a2fcfaaf510b6503df5490d6d8
ankit <ankitarora121@gmail.com>
2019-03-19 07:53:52 +0000

    Declare dependencies in setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='blue_event_framework',
-    version='0.1.0',
+    version='0.1.1',
     author='Coinswitch',
     author_email='dev@coinswitch.co',
     description='dummy description',
     packages=find_packages(),
+    install_requires=[
+        'dataclasses>=0.6',
+        'peewee>=3.9.2',
+        'boto3>=1.7'
+    ]
 )
