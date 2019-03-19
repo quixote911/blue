f60926ffbd00522447168275601c4bc93a9b8e94
ankit <ankitarora121@gmail.com>
2019-03-19 10:10:11 +0000

    Add basic documentation

--- test/test_blue.py
+++ test/test_blue.py
@@ -10,7 +10,43 @@ log = logging.getLogger(__name__)
 
 # initialize_fixtures()
 
+"""
+blue
+------------------------------------
 
+A Blueprint is a set of Instructions
+    An Instruction is a Condition and an Outcome
+        A Condition is a list of Event topics to listen to
+        An Outcome specifes what happens when Condition is satisfied
+
+A BlueprintExecution happens when a Blueprint with an initial Event and an execution_context
+
+How to use: 
+        
+    Initial - 
+        Let BlueprintManager know about Blueprint you're interested to execute
+        Let BlueprintExecutionManager know about BlueprintManager
+    
+    Step 1 - Start a BlueprintExecution
+        Tell BlueprintExecutionManager to execute your Blueprint by supplying the initial Event and execution_context
+        This will queue up Instructions from your Blueprint to execute.
+    
+    Step 2 - Execute the BlueprintExecution
+        Tell BlueprintExecutor about BlueprintExecutionManager.
+        BlueprintExecutor gets all PENDING Instructions
+        It checks EventBus for Events satisfying the Instruction's Condition
+        If Condition Event is found:
+            It calls Outcome's Adapter with execution_context and Event. 
+            Outcome.Adapter's result is fed as input to Outcome.Action
+            Instruction is marked SUCCESS
+        If not found:
+            It doesn't do anything and moves on to the next Instruction
+            Instruction is marked PENDING
+            
+     
+     
+          
+"""
 def basic_initialize_execution_manager(sample_namespace_config, sample_blueprint_definition):
     event_bus = InMemoryEventBus(dict())
     execution_store = InMemoryBlueprintInstructionExecutionStore(dict())
